f98e8b8e3c83769a2f956277d5dc769c072d7a92
Sneha Sakri <snehasakri75@gmail.com>
2025-08-29 21:18:04 +0530

    Shopper project Push

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import './DescriptionBox.css';
+
+const DescriptionBox = () => {
+    return (
+        <div className="descriptionbox">
+            <div className="descriptionbox-navigator">
+              <div className="descriptionbox-nav-box">DescriptionBox</div> 
+                <div className="descriptionbox-nav-box fade">Reviews (122)</div> 
+            </div>
+            <div className="descriptionbox-description">
+                <p>Our clothing e-commerce website is a modern online platform that offers a wide range of fashion apparel for men, women, and kids. From everyday basics to trendy seasonal styles, our website provides a seamless shopping experience with easy navigation, high-quality images, and secure payment options.
+
+Customers can browse various categories such as t-shirts, jeans, dresses, ethnic wear, activewear, and more ,all curated from top brands and local designers. Each product comes with detailed descriptions, size guides, customer reviews, and high-resolution images to help users make informed purchases.
+
+With features like user accounts, order tracking, a wishlist, and a responsive cart system, our site ensures convenience and satisfaction from browsing to checkout. Whether you're looking for casual outfits or stylish partywear, our platform is your go-to destination for fashion at your fingertips.</p>
+                <p>Our clothing e-commerce website is a modern online platform that offers a wide range of fashion apparel for all.A stylish and user-friendly e-commerce website offering a wide range of clothing for all ages. Featuring secure checkout, size guides, and fast delivery, it provides a seamless shopping experience for fashion lovers.</p>
+            </div>
+        </div>
+    )
+}
+
+export default DescriptionBox;
